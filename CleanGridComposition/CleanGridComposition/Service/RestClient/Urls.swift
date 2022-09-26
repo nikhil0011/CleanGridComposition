fa9906f611ff22ac8d64d74a93f52e2ef3c88934
Nikhil <nikhil.nangia85@gmail.com>
2022-09-26 23:59:04 +0400

    Update Urls.swift

--- CleanGridComposition/CleanGridComposition/Service/RestClient/Urls.swift
+++ CleanGridComposition/CleanGridComposition/Service/RestClient/Urls.swift
@@ -9,6 +9,7 @@ import Foundation
 
 let BASEURL =
     "https://run.mocky.io/v3"
+let ERROR_PAGE_URL = URL(string: "www.google.com/404")!
 struct Endpoint {
     static let recipes = "/5c138271-d8dd-4112-8fb4-3adb1b7f689e"
 }
